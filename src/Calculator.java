d158cf6c49fe64da1a4cd1fe8cdcc1aa54cca0f3
chenshuiluke <chenshuiluke@gmail.com>
2016-01-27 00:51:01 -0500

    Added operator buttons and rearrange the number buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -25,42 +25,66 @@ public class Calculator extends Application{
 			Application.launch(args);
 	}
 	public void start(Stage primaryStage){
+		int windowWidth = 95;
+		int windowHeight = 250;
 		primaryStage.setTitle("Calculator");
-		primaryStage.setWidth(100);
-		primaryStage.setHeight(200);
+		primaryStage.setWidth(windowWidth);
+		primaryStage.setHeight(windowHeight);
+		primaryStage.setResizable(false);
 		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root, 100, 200, Color.WHITE);
+		Scene scene = new Scene(root, windowWidth, windowHeight, Color.WHITE);
 
 		TextField equationTextField = new TextField();
 		equationTextField.setEditable(false);
 		root.setTop(equationTextField);
 
-		GridPane calculatorPane = new GridPane();
-		calculatorPane.setHgap(5);
-		calculatorPane.setVgap(5);
-	//	calculatorPane.setGridLinesVisible(true);
+		GridPane numberGrid = new GridPane();
+		numberGrid.setHgap(5);
+		numberGrid.setVgap(5);
+
+		GridPane operatorGrid = new GridPane();
+		operatorGrid.setHgap(5);
+		operatorGrid.setVgap(5);
+/*
+		numberGrid.setGridLinesVisible(true);
+		operatorGrid.setGridLinesVisible(true);
+*/
 
 
-		int buttonCounter = 1;
+		int buttonCounter = 0;
+		int buttonWidth = 500;
+		int buttonHeight = 500;
 		for(int y = 0; y < 3; y++){
 			RowConstraints row = new RowConstraints();
-			row.setPercentHeight(33);
-			calculatorPane.getRowConstraints().add(row);
+			row.setPercentHeight(20);
+			numberGrid.getRowConstraints().add(row);
 
-			ColumnConstraints column = new ColumnConstraints();
-			column.setPercentWidth(33);
-			column.setHalignment(HPos.CENTER);
-			calculatorPane.getColumnConstraints().add(column);
 
-			for(int x = 0; x < 3; x++, buttonCounter++){
+			for(int x = 0; x < 3 && buttonCounter < 10; x++, buttonCounter++){
+				if(y == 0){
+					ColumnConstraints column = new ColumnConstraints();
+					column.setPercentWidth(33);
+					column.setHalignment(HPos.CENTER);
+				}
+
 				Button button = new Button(new String().valueOf(buttonCounter));
-				button.setPrefWidth(500);
-				button.setPrefHeight(500);
-				calculatorPane.add(button, x, y);
+				numberGrid.add(button, x, y);
+			}
+			if(buttonCounter == 9){
+				Button button = new Button(new String().valueOf(buttonCounter));
+				numberGrid.add(button, 0, y+1);
 			}
 		}
+		String[][] operatorTextArr = {{"=", "+"}, {"-", "*"}, {"/", "c"}};
+		for(int y = 0; y < operatorTextArr.length; y++){
+				for(int x = 0; x < operatorTextArr[y].length; x++){
+					Button button = new Button(operatorTextArr[y][x]);
+					operatorGrid.add(button, y, x);
+				}
+		}
 
-		root.setCenter(calculatorPane);
+		root.setCenter(numberGrid);
+		root.setBottom(operatorGrid);
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
